0e0b1842c7c49bcbeda1b9eb66524e6098eafa76
Gabriel <gabrielmurryedh@gmail.com>
2023-02-15 18:34:33 -0800

    trying to make deployment work, fixed uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(
+    `mongodb+srv://GabrielMurry:${process.env.MONGO_DB_PASSWORD}@cluster0.hdvtgs4.mongodb.net/mgmt_db?retryWrites=true&w=majority`
+  );
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold); // cyan... using colors package and this will be colored (in order to use colors, we have to bring it in in our main index file)
 };
