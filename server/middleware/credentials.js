184a837f240127a4c7979c0947afee534e42db42
Gabriel <gabrielmurryedh@gmail.com>
2023-02-17 11:33:44 -0800

    added res.setHeader access control allow origin

--- server/middleware/credentials.js
+++ server/middleware/credentials.js
@@ -3,7 +3,9 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   }
   next();
 };
