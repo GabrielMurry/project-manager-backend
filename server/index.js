4b9d99605224caefe2cb097b545226b0448e626e
Gabriel <gabrielmurryedh@gmail.com>
2023-02-16 16:06:21 -0800

    added access control allow origin

--- server/index.js
+++ server/index.js
@@ -21,10 +21,18 @@ connectDB();
 
 app.use(
   "/graphql",
-  cors({
-    origin: "https://project-manager-frontend-delta.vercel.app",
-    credentials: true,
-  }),
+  function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, Content-Length, X-Requested-With"
+    );
+    if (req.method === "OPTIONS") {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  },
   graphqlHTTP({
     schema: schema,
     graphiql: process.env.NODE_ENV === "development",
