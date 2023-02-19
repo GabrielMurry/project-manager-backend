d8a21af5afaf9b80cd8ed9e89c2abf8cfe41252c
Gabriel <gabrielmurryedh@gmail.com>
2023-02-18 17:59:39 -0800

    trying to make custom page routing work in prod

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
+const path = require("path");
 const corsOptions = require("./config/corsOptions");
 const credentials = require("./middleware/credentials");
 const { graphqlHTTP } = require("express-graphql");
@@ -19,6 +20,9 @@ app.use(credentials);
 
 app.use(cors(corsOptions));
 
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -27,8 +31,15 @@ app.use(
   })
 );
 
-app.get("*", function (req, res) {
-  res.send("public/index.html");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.listen(port, console.log(`Server running on port ${port}`));
